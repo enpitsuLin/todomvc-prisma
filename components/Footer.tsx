c4248d0726386a0dfcbcd7121dc16507b5fe4547
enpitsulin <enpitsulin@gmail.com>
2022-04-30 00:43:11 +0800

    feat: footer info

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,13 @@
 const Footer = () => {
   return (
-    <footer>
+    <footer className="info">
       <p>Double-click to edit a todo</p>
+      <p>
+        Created by <a href="http://github.com/enpitsulin/">enpitsulin</a>
+      </p>
+      <p>
+        Part of <a href="http://todomvc.com">TodoMVC</a>
+      </p>
     </footer>
   )
 }
