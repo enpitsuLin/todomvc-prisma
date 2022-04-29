aaf47aa24fd2c473819e29bef96b29fd3210c044
enpitsulin <enpitsulin@gmail.com>
2022-04-29 21:24:36 +0800

    feat: todos filter

--- components/Todolist.tsx
+++ components/Todolist.tsx
@@ -25,9 +25,15 @@ const TodoList: React.FC<Props> = ({ todos = [] }) => {
           <strong>1</strong>items left
         </span>
         <ul className="filters">
-          <li></li>
-          <li></li>
-          <li></li>
+          <li>
+            <a>All</a>
+          </li>
+          <li>
+            <a>Active</a>
+          </li>
+          <li>
+            <a>Completed</a>
+          </li>
         </ul>
       </footer>
     </>
