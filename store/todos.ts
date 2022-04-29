dfe97dbcdbba3a8ea6c9a0627aecbe6ffed92799
enpitsulin <enpitsulin@gmail.com>
2022-04-29 23:28:04 +0800

    feat: improved undeletedTodos

--- store/todos.ts
+++ store/todos.ts
@@ -5,8 +5,13 @@ export const filterType = atom<'all' | 'active' | 'completed'>('all')
 
 export const todosAtom = atom<Todo[]>([])
 
-export const filterAtom = atom((get) => {
+export const undeletedTodos = atom((get) => {
   const todos = get(todosAtom)
+  return todos.filter((todo) => !todo.isDelete)
+})
+
+export const filterAtom = atom((get) => {
+  const todos = get(undeletedTodos)
   return todos.filter((todo) => {
     if (get(filterType) === 'all') return true
     return todo.done === (get(filterType) === 'completed')
@@ -14,11 +19,11 @@ export const filterAtom = atom((get) => {
 })
 
 export const activeTodoCountAtom = atom((get) => {
-  const todos = get(todosAtom)
+  const todos = get(undeletedTodos)
   return todos.filter((todo) => !todo.done).length
 })
 
 export const anyTodosDone = atom((get) => {
-  const todos = get(todosAtom)
+  const todos = get(undeletedTodos)
   return todos.some((todo) => todo.done)
 })
