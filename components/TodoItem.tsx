65d73f5aa5c3ce724761e89bc03539d32b60bd4a
enpitsulin <enpitsulin@gmail.com>
2022-04-29 23:20:08 +0800

    feat: add toggle done

--- components/TodoItem.tsx
+++ components/TodoItem.tsx
@@ -1,5 +1,7 @@
+import { useAtom } from 'jotai'
 import { useCallback } from 'react'
 import { useRef, useState } from 'react'
+import { todosAtom } from '../store/todos'
 
 export interface Todo {
   label: string
@@ -12,15 +14,26 @@ interface Props {
 }
 
 const TodoItem: React.FC<Props> = ({ todo }) => {
+  const [, setTodos] = useAtom(todosAtom)
   const [editing, setEditing] = useState(false)
   const ref = useRef<HTMLInputElement>(null)
   const finishEditing = useCallback(() => {
     setEditing(false)
   }, [todo])
+  const onDone = useCallback(() => {
+    setTodos((todos) => {
+      return todos.map((t) => {
+        if (t.id === todo.id) {
+          return { ...t, done: !t.done }
+        }
+        return t
+      })
+    })
+  }, [todo.id])
   return (
     <li className={`${editing ? 'editing' : ''} ${todo.done ? 'completed' : ''}`}>
       <div className="view">
-        <input type="checkbox" className="toggle" checked={todo.done} onChange={() => {}} autoFocus />
+        <input type="checkbox" className="toggle" checked={todo.done} onChange={onDone} autoFocus />
         <label>{todo.label}</label>
         <button className="destroy" />
       </div>
