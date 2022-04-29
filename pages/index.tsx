e41e580f76162c5a3ddba2550c7d9ba498639c1f
enpitsulin <enpitsulin@gmail.com>
2022-04-30 00:29:17 +0800

    feat: add favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { useAtom } from 'jotai'
 import { useHydrateAtoms } from 'jotai/utils'
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
+import Head from 'next/head'
 import React from 'react'
 import Footer from '../components/Footer'
 import { Todo } from '../components/TodoItem'
@@ -14,6 +15,14 @@ const Blog: React.FC<InferGetServerSidePropsType<typeof getServerSideProps>> = (
   const [todos] = useAtom(filterAtom)
   return (
     <div>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="theme-color" content="#AF2F2F" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <title>todos</title>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <div className="todoapp">
         <TodoList todos={todos} />
       </div>
