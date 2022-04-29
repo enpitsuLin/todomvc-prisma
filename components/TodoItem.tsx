6ccb2b9f3385cd4c35c2313add294bb1e11fb97c
enpitsulin <enpitsulin@gmail.com>
2022-04-30 00:07:30 +0800

    feat: add react-use and some TodoItem event

--- components/TodoItem.tsx
+++ components/TodoItem.tsx
@@ -1,8 +1,9 @@
 import { useAtom } from 'jotai'
-import { useCallback } from 'react'
+import { KeyboardEventHandler, useCallback } from 'react'
 import { useRef, useState } from 'react'
 import { todosAtom } from '../store/todos'
 import { useDebouncedCallback } from 'use-debounce'
+import { useClickAway } from 'react-use'
 
 export interface Todo {
   label: string
@@ -15,17 +16,47 @@ export interface Todo {
 interface Props {
   todo: Todo
 }
+function useDoubleClick(onClick: any, onDoubleClick: any) {
+  let clicks: number[] = []
+  let timeout: number
 
+  return (event: any) => {
+    clicks.push(new Date().getTime())
+
+    clearTimeout(timeout)
+    timeout = window.setTimeout(() => {
+      if (clicks.length > 1 && clicks[clicks.length - 1] - clicks[clicks.length - 2] < 250) {
+        if (onDoubleClick) {
+          onDoubleClick(event)
+        }
+      } else if (onClick) {
+        onClick(event)
+      }
+      clicks = []
+    }, 250)
+  }
+}
 const TodoItem: React.FC<Props> = ({ todo }) => {
   const [, setTodos] = useAtom(todosAtom)
   const [editing, setEditing] = useState(false)
   const ref = useRef<HTMLInputElement>(null)
+
+  useClickAway(ref, () => {
+    finishEditing()
+  })
+
   const finishEditing = useCallback(() => {
     setEditing(false)
   }, [todo])
+
+  const handleViewClick = useDoubleClick(null, () => {
+    setEditing(true)
+  })
+
   const toggleDone = useDebouncedCallback((id: number, done: boolean) => {
     fetch('/api/todo', { method: 'POST', body: JSON.stringify({ id, done: !done }) })
   }, 1000)
+
   const onDone = useCallback(() => {
     setTodos((todos) => {
       return todos.map((t) => {
@@ -37,16 +68,40 @@ const TodoItem: React.FC<Props> = ({ todo }) => {
       })
     })
   }, [todo.id])
+
+  const onEnter = useCallback(
+    ((event) => {
+      if (event.key === 'Enter') {
+        event.preventDefault()
+        finishEditing()
+      }
+    }) as KeyboardEventHandler<HTMLInputElement>,
+    [todo]
+  )
+
   return (
-    <li className={`${editing ? 'editing' : ''} ${todo.done ? 'completed' : ''}`}>
+    <li className={`${editing ? 'editing' : ''} ${todo.done ? 'completed' : ''}`} onClick={handleViewClick}>
       <div className="view">
         <input type="checkbox" className="toggle" checked={todo.done} onChange={onDone} autoFocus />
         <label>{todo.label}</label>
         <button className="destroy" />
       </div>
-      {editing && <input ref={ref} type="text" className="edit" />}
+      {editing && (
+        <input
+          ref={ref}
+          type="text"
+          autoFocus={true}
+          value={todo.label}
+          onChange={() => {}}
+          className="edit"
+          onKeyPress={onEnter}
+        />
+      )}
     </li>
   )
 }
 
 export default TodoItem
+function DOMAttributes<T>() {
+  throw new Error('Function not implemented.')
+}
