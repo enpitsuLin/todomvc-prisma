caa55c62a0329f1a30121332239b7be9e6cd983e
enpitsulin <enpitsulin@gmail.com>
2022-04-30 14:35:22 +0800

    fix: global prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,16 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 
 // PrismaClient is attached to the `global` object in development to prevent
 // exhausting your database connection limit.
 //
-// Learn more: 
+// Learn more:
 // https://pris.ly/d/help/next-js-best-practices
+
+declare global {
+  // allow global `var` declarations
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined
+}
 
 let prisma: PrismaClient
 
